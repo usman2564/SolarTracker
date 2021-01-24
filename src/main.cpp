2dbdd50fcbad94ba45a28b82ce1cd19c3552f485
usman2564 <usmanqureshi2464@gmail.com>
2021-01-23 22:46:15 -0700

    changed to esp32 feather, cleaned code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <Arduino.h>
 
+
 //Solar Tracker Project- By USMAN QURESHI
 //##################################################################################################################################
+
+#define MAX_SERVO_ANGLE 180
+#define MIN_SERVO_ANGLE 0
+#define DEFAULT_ANGLE 90
+#define LIGHT_INTENSITY_THRESHOLD 150
+#define DEGREE_INCREMENT 2
+#define DEGREE_DECREMENT -2
 
 //Photo-Resistor Class
 class LDR
@@ -14,7 +22,7 @@ public:
   }
   
   //function that reads the value of the LDR, and returns that value
-  int read()
+  int lightIntensity()
   {
     return analogRead(photoPin);
   }
@@ -42,116 +50,97 @@ public:
 
   //function that controls movement of servo; using specs on HS-422 data sheet, (925/90)
   //is a conversion value that allows a degree-based control.
-  void sweep(int deg)
+  void setAngle(int deg)
   {
     //save input of degree the servo should be at
-    sweepDegree = deg;
+    currentAngle = deg;
+
+    if( currentAngle > MAX_SERVO_ANGLE )
+    {
+      currentAngle = MAX_SERVO_ANGLE;
+      return;
+    }
+    else if( currentAngle < MIN_SERVO_ANGLE ) {
+      currentAngle = MIN_SERVO_ANGLE;
+      return;
+    }
 
     //artificial PWM control of servo, specifically HS-422
     digitalWrite(servoPin, HIGH);
-    delayMicroseconds(575 + (925 / 90) * sweepDegree);
+    delayMicroseconds(575 + (925 / 90) * currentAngle);
     digitalWrite(servoPin, LOW);
     delayMicroseconds(20000);
   }
 
+  void rotate(int deg)
+  {
+    setAngle(currentAngle + deg);    
+  }
+
+  int angle()
+  {
+    return currentAngle;
+  }
+
 //function that will turn off the servo
-void servoStop() 
+void stop() 
 {
   digitalWrite(servoPin,LOW);
 }
+
+void updateUsingLightSensor(LDR& lightSensorA, LDR& lightSensorB)
+{
+  if (abs(lightSensorA.lightIntensity() - lightSensorB.lightIntensity()) < LIGHT_INTENSITY_THRESHOLD)
+  {
+    stop();
+  }
+  else if (lightSensorA.lightIntensity() < lightSensorB.lightIntensity())
+  {
+    rotate(DEGREE_INCREMENT);
+  }
+  else
+  {
+    rotate(DEGREE_DECREMENT);
+  }
+}
+
 private:
-  //the servo pin and the degree the servo will sweep to is private
+  //the servo pin and the degree the servo will rotate to is private
   int servoPin;
-  int sweepDegree;
+  int currentAngle;
 };
 
 //attach 4 LDRS on A0 A1 A2 A3 and two servos on pin 4 and 5
-LDR photo1(1);
-LDR photo2(2);
-LDR photo3(0);
-LDR photo4(3);
-Servo servo(4);
-Servo servo1(5);
+LDR topLightSensor(15);
+LDR bottomLightSensor(32);
+LDR rightLightSensor(14);
+LDR leftLightSensor(33);
+Servo topServo(12);
+Servo bottomServo(13);
 
 //initialize the servo to 90 degree, and set a 70 error margin with photo-resistors. servo1direction dictates how servo1 will
 //move WRT the angle of servo
-int servoAngle = 90;
-int servo1Angle = 90;
-int servo1direction = 0;
 
 void setup()
 {
   //initialize the servo, and wait 10s before functioning the main program.
-  servo.initialize();
-  servo1.initialize();
-  servo.sweep(servoAngle);
-  servo1.sweep(servo1Angle);
-  //Serial.begin(9600);
-  delay(10000);
+  topServo.initialize();
+  bottomServo.initialize();
+  topServo.setAngle(DEFAULT_ANGLE);
+  bottomServo.setAngle(DEFAULT_ANGLE);
 }
 
 void loop()
 {
+ topServo.updateUsingLightSensor(topLightSensor, bottomLightSensor);
 
-  //check with LDR is brighter. The lesser value will be brighter.
-  if (abs(photo1.read() - photo2.read()) < 25)
+  if (topServo.angle() < 90)
   {
-    servo.servoStop();
+    bottomServo.updateUsingLightSensor(leftLightSensor, rightLightSensor);
   }
-  else if (photo1.read() < photo2.read())
+  else
   {
-    //check to make sure the angle doesnt pass 180 degree
-    if (servoAngle != 180)
-    {
-      //change the servo's angle slightly (CW) and move it according to the brightness
-      servoAngle = servoAngle + 2;
-      servo.sweep(servoAngle);
-    }
-  }
-  else if (photo2.read() < photo1.read())
-  {
-    //make sure the angle doesnt fall below 0 degree
-    if (servoAngle != 0)
-    {
-      //change the servo's angle slightly (CCW) and move it according to the brightness
-      servoAngle = servoAngle - 2;
-      servo.sweep(servoAngle);
-    }
-  }
-
-//servo1direction changes WRT servoAngle
-if (servoAngle > 0 && servoAngle < 90)
-{
-  servo1direction = -1;
-} else if (servoAngle > 90 && servoAngle < 180) {
-  servo1direction = 1;
-}
-
-
-//check with LDR is brighter. The lesser value will be brighter.
-  if(abs(photo3.read() - photo4.read()) < 25)
-  {
-    servo1.servoStop();
-  }
-  else if ((photo3.read() < photo4.read() && servo1direction == 1) || (photo4.read() < photo3.read() && servo1direction == -1))
-  {
-    //check to make sure the angle doesnt pass 180 degree
-    if (servo1Angle != 0)
-    {
-      //change the servo's angle slightly and move it according to the brightness
-      servo1Angle = servo1Angle - 2;
-      servo1.sweep(servo1Angle);
-    }
-  }
-  else if ((photo3.read() < photo4.read() && servo1direction == -1) || (photo4.read() < photo3.read() && servo1direction == 1))
-  {
-    //make sure the angle doesnt fall below 0 degree
-    if (servo1Angle != 180)
-    {
-      //change the servo's angle slightly and move it according to the brightness
-      servo1Angle = servo1Angle + 2;
-      servo1.sweep(servo1Angle);
-    }
+    bottomServo.updateUsingLightSensor(rightLightSensor, leftLightSensor);
   }
 
   //wait 40ms before checking again
